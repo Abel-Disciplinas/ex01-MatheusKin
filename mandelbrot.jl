3a516a78e13764874b3e38412bb86d567a9ea4b0
MatheusKin <48693177+MatheusKin@users.noreply.github.com>
2019-03-21 10:47:23 -0300

    Add files via upload

--- mandelbrot.jl
+++ mandelbrot.jl
@@ -4,15 +4,25 @@
 using Images
 
 # z^2 = x^2 + 2xyi - y^2 = (x^2 - y^2) + (2xy) i
+function quadrado_complex(x,y)
+  x, y = (x^2 - y^2), 2*x*y
+  return x, y
+end
 function iteracao_mandelbrot(cx, cy; max_iter = 40)
   iter = 0
 
-  # AQUI
+  x, y = 0.0, 0.0     #x é parte real, y é parte imaginária
+  while iter ≤ max_iter
+    x, y = quadrado_complex(x,y)
+    x, y = x + cx, y + cy
+    iter += 1
+  end
+
 
   if x^2 + y^2 < 4
     return false, 0
   else
-    return true, iter / max_iter
+    return true, round(iter / max_iter)   #O ERRO TAVA AQUI AONDE NÃO ERA PRA MEXER
   end
 end
 
